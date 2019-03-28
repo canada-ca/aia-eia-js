cdc352c9f4ffa3da1f31895685869ec29d219754
Guillaume Charest <guillaume.w.charest@gmail.com>
2019-03-28 14:49:20 -0400

    Update vue.config.js with production env

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,10 @@
 module.exports = {
   //THere are some known issues with multi-page apps
   //jand using HTML history.pushState routing
-  publicPath: "./",
+  /*publicPath: "./",*/
+  publicPath: process.env.NODE_ENV === 'production' ? 
+    '/aia-eia-js/' :
+    '/',
 
   configureWebpack: {
     devtool: "source-map"
