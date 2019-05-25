df256fe69d81a50651b6bcab162372256ba49f49
Guillaume Charest <guillaume.charest@tbs-sct.gc.ca>
2019-05-24 22:31:11 -0400

    Fix keys (#89)
    * Fix keys to match question IDs
    
    * Add French terms
    
    * Remove comment

--- src/components/Obligations.vue
+++ src/components/Obligations.vue
@@ -22,8 +22,6 @@
 </template>
 
 <script lang="ts">
-//Display the obligations related to the score level
-//See http://www.tbs-sct.gc.ca/pol/doc-fra.aspx?id=32592#appC for obligations
 import Vue from "vue";
 import i18n from "@/plugins/i18n";
 import Component from "vue-class-component";
