78b395b6530c4eb83139a9bb8d080cd73acdb956
CalvinRodo <calvinv@gmail.com>
2019-05-25 23:52:54 -0400

    Formatting test

--- tests/unit/i18n.spec.ts
+++ tests/unit/i18n.spec.ts
@@ -1,4 +1,3 @@
-
 import en from "@/plugins/en.json";
 import fr from "@/plugins/fr.json";
 import _ from "lodash";
@@ -9,19 +8,19 @@ const frKeys = Object.keys(fr);
 describe("i18n", () => {
   it("should have identical keys", () => {
     const val = _.difference(enKeys, frKeys);
-    const enIntersection = _.intersection(enKeys, val);
-    const mek = enIntersection.join("\n")
-    const frIntersection = _.intersection(frKeys, val);
-    const mfk  = frIntersection.join("\n");
-    
-    console.log(`
-extra keys in en.json
-${mek}
-extra keys in fr.json
-${mfk}`);
-    
+    if (val.length > 0) {
+
+      const mek = _.intersection(enKeys, val)
+                   .join("\n");
+
+      const mfk = _.intersection(frKeys, val)
+                   .join("\n");
+
+      console.log(
+        `extra keys in en.json\n ${mek} extra keys in fr.json\n ${mfk}`
+      );
+    }
+
     expect(Object.keys(val).length).toBe(0);
   });
-  
-
 });
