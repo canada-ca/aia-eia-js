7ab04daaee1003a55e812df2b028443e3f32ea92
Dave Dumaresq <dave@educationplannerbc.ca>
2019-08-11 17:32:26 -0700

    Add Bootstrap Vue

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,12 @@ import store from "./store";
 import i18n from "@/plugins/i18n";
 import "./registerServiceWorker";
 import { StylesManager } from "survey-vue";
+import BootstrapVue from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
 
 Vue.config.productionTip = false;
+Vue.use(BootstrapVue);
 
 StylesManager.applyTheme("bootstrap");
 
