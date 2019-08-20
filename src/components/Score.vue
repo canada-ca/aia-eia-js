91a6499a4ccacffe3796f2f5e23a293db83596d9
Dave Dumaresq <dave@educationplannerbc.ca>
2019-08-19 20:51:52 -0700

    Use acronyms for score label on mobile screens

--- src/components/Score.vue
+++ src/components/Score.vue
@@ -16,11 +16,11 @@
       <b-col>{{ $t("rawRiskScore") }}: {{ score[0] }}</b-col>
       <b-col>{{ $t("mitigationScore") }}: {{ score[1] }}</b-col>
     </b-row>
-      <b-row :class="alertclass" :no-gutters="true" v-if="isMobile()">
-      <b-col>IL: {{ score[3] }}</b-col>
-      <b-col>CS: {{ score[2] }}</b-col>
-      <b-col>RS: {{ score[0] }}</b-col>
-      <b-col>MS: {{ score[1] }}</b-col>
+    <b-row :class="alertclass" :no-gutters="true" v-if="isMobile()">
+      <b-col>{{ $t("IL") }}: {{ score[3] }}</b-col>
+      <b-col>{{ $t("CS") }}: {{ score[2] }}</b-col>
+      <b-col>{{ $t("RS") }}: {{ score[0] }}</b-col>
+      <b-col>{{ $t("MS") }}: {{ score[1] }}</b-col>
     </b-row>
   </b-container>
 </template>
@@ -32,18 +32,15 @@ import { Getter } from "vuex";
 // define a mixin object
 var myMixin = {
   methods: {
-    isMobile: function () {
-      if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-        console.log('Is Mobile');
+    isMobile: function() {
+      if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
         return true;
       } else {
-        console.log('Not Mobile');
         return false;
       }
     }
   }
-}
-
+};
 
 @Component({
   mixins: [myMixin],
@@ -57,14 +54,8 @@ var myMixin = {
       if (score === 2) return "scoreClass alert alert-info";
       if (score === 3) return "scoreClass alert alert-warning";
       if (score === 4) return "scoreClass alert alert-danger";
-      /*const score = this.$store.getters.calcScore[2];
-      if (score <= 18) return "alert alert-success";
-      if (score > 18 && score <= 36) return "alert alert-info";
-      if (score > 36 && score <= 54) return "alert alert-warning";
-      if (score > 54) return "alert alert-danger";*/
     }
   }
 })
-
 export default class Score extends Vue {}
 </script>
