064297cbb790b7dfd015adb1cf0e9d23480a6054
ShadeWyrm <vivian.nobrega@tbs-sct.gc.ca>
2020-03-06 11:33:18 -0500

    * Initial French localization at the end of result

--- src/components/TextResult.vue
+++ src/components/TextResult.vue
@@ -4,7 +4,28 @@
       <strong v-if="locale == undefined">{{ data.title }}</strong>
       <strong v-if="locale !== undefined">{{ data.titleData[locale] }}</strong>
       <br />
-      {{ data.displayValue }}
+      <div v-if="locale == undefined">
+        <div>
+          <strong v-if="$root.$i18n.locale == 'en'" class="label label-default">{{ $t("englishContent") }} : </strong>
+          <strong v-if="$root.$i18n.locale == 'fr'" class="label label-default">{{ $t("frenchContent") }} : </strong>
+          <p>{{ data.displayValue }}</p>
+        </div>
+        <div>
+          <strong v-if="$root.$i18n.locale == 'en'" class="label label-default">{{ $t("frenchContent") }} : </strong>
+          <strong v-if="$root.$i18n.locale == 'fr'" class="label label-default">{{ $t("englishContent") }} : </strong>
+          <div>
+            <textarea v-model="data.displayValueAlt"></textarea>
+          </div>
+        </div>
+      </div>
+      <div v-if="locale !== undefined">
+        <p v-if="locale == $root.$i18n.locale">
+          {{ data.displayValue }}
+        </p>
+        <p v-if="locale != $root.$i18n.locale">
+          {{ data.displayValueAlt }}
+        </p>
+      </div>
     </div>
   </div>
 </template>
