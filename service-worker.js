d5b9297a2f0451d9d8b574d4eb3a1edb47dce113
ShadeWyrm <ShadeWyrm@users.noreply.github.com>
2020-03-30 12:17:34 +0000

    deploy: 2a1bba0c581433c42392a648e1db052803e8aba9

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/aia-eia-js/precache-manifest.0c0380bac0483db66108cc22612f4dba.js"
+  "/aia-eia-js/precache-manifest.3bfd56ad34d1406c7dc0afa1432fbb34.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "aia-eia-js"});
