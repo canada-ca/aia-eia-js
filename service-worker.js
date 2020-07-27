7e0e126966e0893d8f6bad34884572fa0bb042ad
gcharest <gcharest@users.noreply.github.com>
2020-07-27 22:27:26 +0000

    deploy: 58398530b0ffa8164ef6ed7f90f2da1be093bfae

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/aia-eia-js/precache-manifest.d3c3f72ded41f81b88f65a450f09e0e6.js"
+  "/aia-eia-js/precache-manifest.59c1abaa58fab07ffab32136445b67bc.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "aia-eia-js"});
