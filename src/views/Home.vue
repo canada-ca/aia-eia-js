bb031f66ce8567b1de476f21104e426ec721a032
Nick Schonning <nschonni@gmail.com>
2020-11-24 18:31:10 -0500

    fix: Unquoted string to read i18n value

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,7 +5,7 @@
       <a
         class="btn btn-default pull-right"
         role="button"
-        :href="$t(linkProjectAnchor)"
+        :href="$t('linkProjectAnchor')"
       >
         {{ $t("linkProjectText") }}
       </a>
