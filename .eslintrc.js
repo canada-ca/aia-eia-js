6333bf56595a222a4228c91afee212d8174ae06a
Nick Schonning <nschonni@gmail.com>
2020-11-30 18:26:36 -0500

    Revert "chore: Use ESLint overrides for file types"
    This reverts commit c4083ed8f1c0b4b5614c823ac2501418a7d59bda.

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,30 +2,20 @@ module.exports = {
   root: true,
   env: {
     node: true,
-    browser: true,
   },
   plugins: ["security", "prettier"],
   extends: [
     "eslint:recommended",
     "plugin:security/recommended",
-    "plugin:prettier/recommended",
+    "plugin:vue/essential",
+    "@vue/prettier",
+    "@vue/typescript",
   ],
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
   },
-  overrides: [
-    {
-      files: ["**/*.ts"],
-      parser: "@typescript-eslint/parser",
-    },
-    {
-      files: ["**/*.vue"],
-      parser: "vue-eslint-parser",
-      parserOptions: {
-        parser: "@typescript-eslint/parser",
-      },
-      extends: ["plugin:vue/recommended", "@vue/typescript", "@vue/prettier"],
-    },
-  ],
+  parserOptions: {
+    parser: "@typescript-eslint/parser",
+  },
 };
