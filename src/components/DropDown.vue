433e6cd0f199acf4ecb05bb455c9237076092fe7
Salim Erradi <erradisalim@gmail.com>
2020-12-07 11:56:40 -0500

    Drop-Down Menu
    Fixes #366

---
+++ src/components/DropDown.vue
@@ -0,0 +1,111 @@
+<template>
+  <div id="dropDown" v-if="displayDropDown == true">
+    <label> {{ $t("navigateSectionLabel") }} </label>
+
+    <div style="margin-top: -10px; margin-left: -55px">
+      <ul>
+        <b-col md="3">
+          <!-- Calls redirectToPage function whenever user changes index -->
+          <select class="dropDownList" @change="redirectToPage()">
+            <option select>{{ $t("selectSection") }} </option>
+            <option v-for="page in totalPages" :key="page.num">
+              {{ "Section " + page.num + ": " + page.title }}
+            </option>
+          </select>
+        </b-col>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+import { Model } from "survey-vue";
+import i18n from "@/plugins/i18n";
+
+@Component
+export default class DropDown extends Vue {
+  @Prop() public survey?: Model;
+  @Prop() displayDropDown?: boolean;
+
+  redirectToPage() {
+    //colon value used to determine weather to set new value as single digit number or double digit
+    var colon = ":";
+    var e = document.getElementsByClassName(
+      "dropDownList"
+    )[0] as HTMLSelectElement;
+    //Used to get selected item
+    var pageSection = e.options[e.selectedIndex].text;
+    var newVal: any;
+    newVal = pageSection[8];
+    //Used to check to see if it is a double digit number to set as newVal which will redirect to specific page
+    if (pageSection[9] != colon) {
+      newVal = pageSection[8] + pageSection[9];
+    }
+    //To redirect to correct page
+    if (this.survey != null) {
+      this.survey.currentPageNo = newVal - 1;
+    }
+  }
+  data() {
+    return {
+      totalPages: [
+        {
+          title: this.$t("resultSectionPD"),
+          num: 1
+        },
+        {
+          title: this.$t("sectionBusinessDriverImpact"),
+          num: 2
+        },
+        {
+          title: this.$t("riskProfile"),
+          num: 3
+        },
+        {
+          title: this.$t("projectAuthority"),
+          num: 4
+        },
+        {
+          title: this.$t("aboutTheSystem"),
+          num: 5
+        },
+        {
+          title: this.$t("aboutTheAlgorithm"),
+          num: 6
+        },
+        {
+          title: this.$t("aboutDecision"),
+          num: 7
+        },
+        {
+          title: this.$t("impactAssessment"),
+          num: 8
+        },
+        {
+          title: this.$t("aboutTheData"),
+          num: 9
+        },
+        {
+          title: this.$t("consultations"),
+          num: 10
+        },
+
+        {
+          title: this.$t("deRiskingAndMitigationMeasuresDQ"),
+          num: 11
+        },
+
+        {
+          title: this.$t("deRiskingAndMitigationMeasuresPF"),
+          num: 12
+        },
+        {
+          title: this.$t("deRiskingAndMitigationMeasuresP"),
+          num: 13
+        }
+      ]
+    };
+  }
+}
+</script>
