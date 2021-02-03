6305c4c60f22d086790bd1e4418ebd0c622655de
Guillaume Charest <guillaume.w.charest@gmail.com>
2021-02-03 10:06:52 -0500

    Create Recommendations.vue

---
+++ src/components/Recommendations.vue
@@ -0,0 +1,14 @@
+<template>
+  <div></div>
+</template>
+<script lang="ts">
+import { Vue, Component, Prop } from "vue-property-decorator";
+@Component({
+  components: {
+    Recommendations
+  }
+})
+export default class Recommendations extends Vue {
+  @Prop() public recommendation!: string;
+}
+</script>
