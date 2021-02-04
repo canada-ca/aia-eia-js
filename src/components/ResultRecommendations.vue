24b7902f5ee5ea8bc932e1803edaba109ff7b754
Guillaume Charest <guillaume.w.charest@gmail.com>
2021-02-03 20:33:44 -0500

    Update name

---
+++ src/components/ResultRecommendations.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    Section name {{ sectionName }} t {{ myRecommendations }}
+
+    <div v-for="recommendation in recommendations" :key="recommendation.id">
+      <div v-if="recommendation.level == '1'">
+        {{ recommendation.title.en }}
+      </div>
+    </div>
+    <b-card-body>
+      <div></div>
+    </b-card-body>
+  </div>
+</template>
+<script lang="ts">
+import i18n from "@/plugins/i18n";
+import { Vue, Component, Prop } from "vue-property-decorator";
+import { Recommendations } from "@/types";
+@Component({
+  components: {
+    ResultRecommendations
+  },
+  computed: {
+    locale() {
+      return i18n.locale;
+    },
+    recommendations() {
+      return this.$store.state.recommendations.sectionsRecommendations
+        .recommendations;
+    }
+  },
+  methods: {}
+})
+export default class ResultRecommendations extends Vue {
+  @Prop() public myRecommendations!: Recommendations;
+  @Prop() public sectionName!: string;
+}
+</script>
