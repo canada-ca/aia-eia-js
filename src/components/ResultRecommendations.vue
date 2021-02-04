f9d4f2542c5975513e5c1440dd3985baef16b59a
Guillaume Charest <guillaume.w.charest@gmail.com>
2021-02-04 16:33:56 -0500

    Create method to get recommendations

--- src/components/ResultRecommendations.vue
+++ src/components/ResultRecommendations.vue
@@ -1,34 +1,44 @@
 <template>
   <div>
     <b-card-body>
-      <div v-for="recommendation in myRecommendations" :key="recommendation.id">
-        <div v-for="rec in recommendation" :key="rec.id">
-          <div v-if="rec.name == sectionName">
-            {{ rec.recommendations[sectionScoreLevel].title.en }}
-          </div>
-        </div>
-      </div>
+      <p>
+        {{
+          getRecommendation(sectionRecommendations, sectionName, "0").title[
+            locale
+          ]
+        }}
+      </p>
     </b-card-body>
   </div>
 </template>
 <script lang="ts">
-import i18n from "@/plugins/i18n";
+import { SectionRecommendation } from "@/types.ts";
 import { Vue, Component, Prop } from "vue-property-decorator";
-import { Recommendations } from "@/types";
 @Component({
   components: {
     ResultRecommendations
   },
-  computed: {
-    locale() {
-      return i18n.locale;
+  computed: {},
+  methods: {
+    /**
+     * Gets the recommendations based on the section name provided and the level achieved in the section
+     */
+    getRecommendation(
+      sectionRecommendations: SectionRecommendation[],
+      sectionName: string,
+      scoreLevel: string
+    ) {
+      let sectionRec = sectionRecommendations.find(section => {
+        return section.name == sectionName;
+      });
+      return sectionRec?.recommendations[parseInt(scoreLevel)];
     }
-  },
-  methods: {}
+  }
 })
 export default class ResultRecommendations extends Vue {
-  @Prop() public myRecommendations!: Recommendations;
+  @Prop() public sectionRecommendations?: SectionRecommendation;
   @Prop() public sectionName!: string;
   @Prop() public sectionScoreLevel!: number;
+  @Prop() public locale!: any;
 }
 </script>
