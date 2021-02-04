583faf147fb50b668d98f68216e17031b588dc99
Guillaume Charest <guillaume.w.charest@gmail.com>
2021-02-03 20:59:32 -0500

    Cleanup section results card display

--- src/components/ResultsCard.vue
+++ src/components/ResultsCard.vue
@@ -1,14 +1,16 @@
 <template>
   <b-card>
-    <b-card-header>{{ section.sectionName }}</b-card-header>
+    <b-card-header>{{ sectionName }}</b-card-header>
     <b-card-title>
-      <div v-for="question in section.questionsNames" :key="question.id">
-        {{ question }}<br />
-      </div>
+      <div v-for="question in questionsNames" :key="question.id"></div>
+      Current score: {{ userScore }}<br />
     </b-card-title>
     <b-card-body>
-      Current score: {{ section.userScore }}<br />
-      Recommendations:
+      <ResultRecommendations
+        :section-name="sectionName"
+        :section-score-level="sectionScoreLevel"
+        :my-recommendations="myRecommendations"
+      />
     </b-card-body>
   </b-card>
 </template>
@@ -17,13 +19,26 @@
 import { Component, Vue, Prop } from "vue-property-decorator";
 import { Survey, Model, Question } from "survey-vue";
 import i18n from "@/plugins/i18n";
-import { Section } from "@/types";
+import { Section, Recommendations } from "@/types";
+import ResultRecommendations from "@/components/ResultRecommendations.vue";
 
 @Component({
-  components: { ResultsCard },
-  computed: {}
+  components: { ResultsCard, ResultRecommendations },
+  computed: {
+    //TODO:sectionScoreLevel--> returns score based on userScore/maxScore
+    sectionScoreLevel() {
+      return 0;
+    }
+  },
+  methods: {}
 })
 export default class ResultsCard extends Vue {
+  @Prop() public sectionName!: string;
+  @Prop() public enabled!: boolean;
+  @Prop() public questionsNames!: string[];
+  @Prop() public userScore!: number;
+  @Prop() public questions!: Question[];
   @Prop() public section!: Section;
+  @Prop() public myRecommendations!: Recommendations;
 }
 </script>
