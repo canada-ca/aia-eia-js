3cf65e549d6223baabc3b13eecf7108e356931a1
Guillaume Charest <guillaume.w.charest@gmail.com>
2021-02-08 20:36:26 -0500

    Create HomeSectionsContainer

---
+++ src/components/HomeSectionsContainer.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <b-card-group deck>
+      <div v-for="section in sections" :key="section.id">
+        <HomeSectionCard :section="section" />
+      </div>
+    </b-card-group>
+  </div>
+</template>
+<script lang="ts">
+import { Vue, Component, Prop } from "vue-property-decorator";
+import HomeSectionCard from "@/components/HomeSectionCard.vue";
+import { Section } from "@/types";
+import { PageModel } from "survey-vue";
+
+@Component({
+  components: { HomeSectionCard },
+  computed: {}
+})
+export default class HomeSectionsContainer extends Vue {
+  @Prop() public sections!: PageModel[];
+}
+</script>
