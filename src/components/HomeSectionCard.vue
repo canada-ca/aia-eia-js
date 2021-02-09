99e0872ddae43670be2f07b0d8a295cfcc8555a7
Guillaume Charest <guillaume.w.charest@gmail.com>
2021-02-08 20:36:42 -0500

    Create HomeSectionCard

---
+++ src/components/HomeSectionCard.vue
@@ -0,0 +1,22 @@
+<template>
+  <b-card
+    :title="section.title"
+    img="../assets/logo.png"
+    img-alt="Image"
+    img-top
+  >
+    <b-card-text>Start this section!</b-card-text>
+    <b-card-footer>This section's status will be displayed here</b-card-footer>
+  </b-card>
+</template>
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+import { PageModel } from "survey-vue";
+
+@Component({
+  components: {}
+})
+export default class HomeSectionCard extends Vue {
+  @Prop() public section!: PageModel;
+}
+</script>
