3c574c10802ba2c7cefd3a5213e1d96eb6d9cdef
Guillaume Charest <guillaume.w.charest@gmail.com>
2021-02-10 16:47:31 -0500

    Add button to each section card + min-width card size

--- src/components/HomeSectionCard.vue
+++ src/components/HomeSectionCard.vue
@@ -1,22 +1,39 @@
 <template>
   <b-card
     :title="section.title"
+    footer="This section's status"
     img="../assets/logo.png"
     img-alt="Image"
     img-top
+    style="min-width: 30rem"
   >
-    <b-card-text>Start this section!</b-card-text>
-    <b-card-footer>This section's status will be displayed here</b-card-footer>
+    <b-card-text>{{ section.description }} </b-card-text>
+    <button
+      type="button"
+      class="btn survey-button"
+      style="width: inherit"
+      v-on:click="goToSection(section.name)"
+      :survey="survey"
+    >
+      {{ $t("navigation.launchThisSection") }}
+    </button>
   </b-card>
 </template>
 <script lang="ts">
 import { Component, Prop, Vue } from "vue-property-decorator";
-import { PageModel } from "survey-vue";
+import { PageModel, SurveyModel } from "survey-vue";
 
 @Component({
   components: {}
 })
 export default class HomeSectionCard extends Vue {
   @Prop() public section!: PageModel;
+  @Prop() public survey!: SurveyModel;
+
+  goToSection(sectionName: string) {
+    this.survey.currentPage = sectionName;
+    this.$store.commit("updateSurveyData", this.survey);
+    this.$router.push("questions");
+  }
 }
 </script>
