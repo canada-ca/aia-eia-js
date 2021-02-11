a0f7996eb46e34374f72ccc3a85685db5776cd03
Guillaume Charest <guillaume.w.charest@gmail.com>
2021-02-10 19:03:27 -0500

    Add scrollBehavior

--- src/router.ts
+++ src/router.ts
@@ -9,6 +9,9 @@ Vue.use(Router);
 export default new Router({
   mode: "history",
   base: process.env.BASE_URL,
+  scrollBehavior() {
+    return { x: 0, y: 0 };
+  },
   routes: [
     {
       path: "/",
