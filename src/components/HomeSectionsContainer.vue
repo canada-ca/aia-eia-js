396b211aca007a85480f5fcb6f146fa83b48b6e9
Guillaume Charest <guillaume.w.charest@gmail.com>
2021-02-17 09:50:27 -0500

    Add icon to component call

--- src/components/HomeSectionsContainer.vue
+++ src/components/HomeSectionsContainer.vue
@@ -6,6 +6,7 @@
         :key="section.id"
         :section="section"
         :survey="survey"
+        :icon="getIcon(section.name, sectionRecommendation)"
       ></HomeSectionCard>
     </b-card-group>
   </div>
@@ -14,12 +15,29 @@
 import { Vue, Component, Prop } from "vue-property-decorator";
 import HomeSectionCard from "@/components/HomeSectionCard.vue";
 import { PageModel, SurveyModel } from "survey-vue";
+import { SectionRecommendation } from "@/types";
 
 @Component({
-  components: { HomeSectionCard }
+  components: { HomeSectionCard },
+  methods: {
+    getIcon(
+      sectionName: string,
+      sectionRecommendation: SectionRecommendation[]
+    ) {
+      let section = sectionRecommendation.find(section => {
+        return section.name === sectionName;
+      });
+      if (section !== undefined) {
+        return section.icon;
+      } else {
+        return "smile";
+      }
+    }
+  }
 })
 export default class HomeSectionsContainer extends Vue {
   @Prop() public sections!: PageModel[];
   @Prop() public survey!: SurveyModel;
+  @Prop() public sectionRecommendation!: SectionRecommendation[];
 }
 </script>
