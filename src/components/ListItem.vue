bd29ce65df7b6f54de3f24f2a9403ad78d684f07
Benoit Deshaies <benoit.deshaies@gmail.com>
2021-11-25 05:51:16 -0500

    add a comment to explain how ListItem works

--- src/components/ListItem.vue
+++ src/components/ListItem.vue
@@ -1,3 +1,19 @@
+<!-- on the results page, used to output the requirements with multiple elements as list items.
+     For example, for the peer review, they can consult a federal expert, a corporation, a university, etc.
+
+     To do this, the text attribute uses \n characters as markers. If none are present, the text
+     is outputted as-is. The list elements are reach separated by \n, and the list starts after \n\n.
+
+     Using the example above, the input could be 
+     "Consulting the appropriate experts, which can include:\n\nA federal expert\nA corporation\nA university"
+
+     which would be outputted as
+          Consulting the appropriate experts, which can include:
+          * A federal expert
+          * A corporation
+          * A university
+-->
+
 <template>
   <div>
     <p>{{ title }}</p>
