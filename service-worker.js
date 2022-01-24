b99a1698d72f7695cdd6c5fe9f1b9697315da7f9
MrDeshaies <MrDeshaies@users.noreply.github.com>
2022-01-24 19:42:20 +0000

    deploy: 5137afda7d31d8d0c965810c297842394ea3e9a9

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/aia-eia-js/precache-manifest.785a3d75b7056dff67f9ef2a51ccc497.js"
+  "/aia-eia-js/precache-manifest.9e04ce797d5e07835512155327764d3d.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "aia-eia-js"});
