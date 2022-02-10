f620b45ded051cd488923206113b3c26de6d93e3
MrDeshaies <MrDeshaies@users.noreply.github.com>
2022-02-10 10:19:38 +0000

    deploy: 4c63b4e8d5998f461f46d3ac18558dffe9fe7b5c

---
+++ precache-manifest.d6b27f619fd28deca80ab3273ceba620.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87fff59c760c77ba20dc0be6083cd238",
+    "url": "/aia-eia-js/404.html"
+  },
+  {
+    "revision": "7d26c9413d22ac2c8814",
+    "url": "/aia-eia-js/css/app.aa35b300.css"
+  },
+  {
+    "revision": "35d5e7d43244ac5d841f",
+    "url": "/aia-eia-js/css/chunk-vendors.c26e9052.css"
+  },
+  {
+    "revision": "227478e3a122d96c53c3f34d2fca5db6",
+    "url": "/aia-eia-js/helper/browser.js"
+  },
+  {
+    "revision": "34dca1724d71a51ed75c91820a012df7",
+    "url": "/aia-eia-js/helper/pdfmake.min.js"
+  },
+  {
+    "revision": "f26b7c03c514d3701f255a4dfe6c0dd1",
+    "url": "/aia-eia-js/helper/startswith.js"
+  },
+  {
+    "revision": "f88fe8576fb527a53c81799d20428ef0",
+    "url": "/aia-eia-js/helper/vfs_fonts.js"
+  },
+  {
+    "revision": "d1c223288343bded74e2fbb2c1c37807",
+    "url": "/aia-eia-js/helper/wet.js"
+  },
+  {
+    "revision": "29f17a6518aac61a603e61fa1254921e",
+    "url": "/aia-eia-js/index.html"
+  },
+  {
+    "revision": "7d26c9413d22ac2c8814",
+    "url": "/aia-eia-js/js/app.60e3e7d7.js"
+  },
+  {
+    "revision": "35d5e7d43244ac5d841f",
+    "url": "/aia-eia-js/js/chunk-vendors.22c24cb1.js"
+  },
+  {
+    "revision": "e3d984ba4da736a0c875e10e88363984",
+    "url": "/aia-eia-js/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/aia-eia-js/robots.txt"
+  },
+  {
+    "revision": "8f471f1df26550d186ba82e7bb5626ee",
+    "url": "/aia-eia-js/survey.css"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/aia-eia-js/precache-manifest.f0287b575d5939fabc6e1f047a3635d9.js"
+  "/aia-eia-js/precache-manifest.d6b27f619fd28deca80ab3273ceba620.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "aia-eia-js"});
