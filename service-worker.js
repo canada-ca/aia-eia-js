d70c035d8b90f847b407ff1562b74726ac8ab28a
ptd-tbs <ptd-tbs@users.noreply.github.com>
2022-04-17 16:12:42 +0000

    deploy: 97539780166f1e00292c1aaa0bc2c75535dc3621

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/aia-eia-js/precache-manifest.7455e0faede924ddb2c3afbb10307825.js"
+  "/aia-eia-js/precache-manifest.50542896799a2b561b5a9fbae046fe5a.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "aia-eia-js"});
