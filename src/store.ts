aca5ce78aa9d31a652e413669b242dc9a78168b1
Benoit Deshaies <benoit.deshaies@gmail.com>
2023-01-28 10:34:24 -0500

    bump version to 0.10.0

--- src/store.ts
+++ src/store.ts
@@ -284,7 +284,7 @@ const store: StoreOptions<RootState> = {
   plugins: [vuexLocal.plugin],
   state: {
     //Added version variable to keep track of current version
-    version: "v0.9.1",
+    version: "v0.10.0",
     answerData: [],
     result: undefined,
     currentPageNo: 0,
