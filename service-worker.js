2c661d454c5fdd6bd366a64da6480c0bd8d04318
nschonni <nschonni@users.noreply.github.com>
2023-02-23 16:13:45 +0000

    deploy: 4dbe9444373200b557a46d57b72396593d80f8fc

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/aia-eia-js/precache-manifest.d9f6df772fd4f8391ad2a14fedd130b4.js"
+  "/aia-eia-js/precache-manifest.2e1332ec6236c01d6a6d9e4e7ef8e24c.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "aia-eia-js"});
