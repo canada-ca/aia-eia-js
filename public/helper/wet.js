98d4499d6b86b33abbda36ecdec291e6e54e6766
Benoit Deshaies <benoit.deshaies@tbs-sct.gc.ca>
2023-03-26 19:39:51 -0400

    change date modified to 2023-03-26

--- public/helper/wet.js
+++ public/helper/wet.js
@@ -33,7 +33,7 @@ function reloadTemplate(){
 
   var defPreFooter = document.getElementById("def-preFooter");
   defPreFooter.outerHTML = wet.builder.preFooter({
-    dateModified: "2023-01-26",
+    dateModified: "2023-03-26",
     versionIdentifier: "0.10.0",
     showPostContent: false,
     showShare: false
